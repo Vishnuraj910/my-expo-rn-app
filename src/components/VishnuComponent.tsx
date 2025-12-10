21029d869917c7673190b858fadaed7dd2bfa92f
Vishnuraj Rajagopal <eyvrajagopal@etihad.ae>
2025-12-10 21:07:51 +0400

    Add tailwind button

--- src/components/VishnuComponent.tsx
+++ src/components/VishnuComponent.tsx
@@ -1,16 +1,17 @@
 import { ReactElement } from 'react';
-import { Alert, Button, View } from 'react-native';
+import { Alert, Text, TouchableOpacity, View } from 'react-native';
 
 export default function VishnuComponent(): ReactElement {
   return (
     <>
       <View className="flex-1 items-center justify-center bg-white">
-        <Button
-          title="Vishnu Component"
+        <TouchableOpacity
+          className="rounded border-b-4 border-blue-700 bg-blue-500 px-4 py-2 font-bold text-white hover:border-blue-500 hover:bg-blue-400"
           onPress={() => {
             Alert.alert('Hello');
-          }}
-        />
+          }}>
+          <Text>Press Me</Text>
+        </TouchableOpacity>
       </View>
     </>
   );
