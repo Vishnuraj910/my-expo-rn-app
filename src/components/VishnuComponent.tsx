f478c3e9833902cb710818d0ec5fe1e7e33b67de
Vishnuraj Rajagopal <eyvrajagopal@etihad.ae>
2025-12-15 14:28:00 +0400

    updated alert text

--- src/components/VishnuComponent.tsx
+++ src/components/VishnuComponent.tsx
@@ -10,7 +10,7 @@ export default function VishnuComponent(): ReactElement {
         <TouchableOpacity
           className="rounded border-b-4 border-blue-700 bg-blue-500 px-4 py-2 font-bold text-white hover:border-blue-500 hover:bg-blue-400"
           onPress={() => {
-            Alert.alert('Hello');
+            Alert.alert('Hello from alert!');
           }}>
           <Text>Press Me</Text>
         </TouchableOpacity>
